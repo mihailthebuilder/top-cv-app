6c15b059f6e28984e52ca7268c4845a346326ad4
Mihail Marian <mihailmarian12@gmail.com>
2020-12-18 09:50:07 +0000

    more styling for form question

--- src/components/LineInput/LineInput.js
+++ src/components/LineInput/LineInput.js
@@ -2,7 +2,7 @@ import "./LineInput.scss";
 
 const LineInput = (props) => {
   return (
-    <form className="line-input">
+    <form className="form-question single-row">
       <label>{props.label}</label>
       <input type={props.inputType} required />
       <button type="submit" className="line-save-button"></button>
