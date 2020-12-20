59ee17a424a40d179835f95b86b26a115227d14b
Mihail Marian <mihailmarian12@gmail.com>
2020-12-20 18:02:05 +0000

    shuffled stylings & added some static bits to Education.js

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -1,12 +1,31 @@
+import React, { Component } from "react";
+
 import "./Education.scss";
 import FormHeading from "../FormHeading/FormHeading.js";
 
-const Education = (props) => {
-  return (
-    <div>
-      <FormHeading title="Education" />
-    </div>
-  );
-};
+class Education extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newEntry: false,
+    };
+
+    this.newEntryButtonPress = this.newEntryButtonPress.bind(this);
+  }
+
+  newEntryButtonPress() {
+    console.log("button pressed!");
+  }
+
+  //NEED TO ADD NEW ENTRY BUTTON HANDLER
+  render() {
+    return (
+      <div>
+        <FormHeading title="Education" />
+      </div>
+    );
+  }
+}
 
 export default Education;
