b38d8eec52253b0cdd8b3b5d1b4011526f1222f6
Mihail Marian <mihailmarian12@gmail.com>
2020-12-20 14:33:22 +0000

    got input save working

--- src/components/LineInput/LineInput.js
+++ src/components/LineInput/LineInput.js
@@ -4,23 +4,24 @@ import SaveButton from "../SaveButton/SaveButton.js";
 const LineInput = (props) => {
   console.log(props.state, props.data);
 
-  let inputJsx = props.data.saved ? (
-    <span>{props.data.value}</span>
-  ) : (
-    <input
-      type={props.type}
-      className="hover-highlight"
-      value={props.data.value}
-      state={props.state}
-      onChange={props.lineInputChange}
-      required
-    />
-  );
+  let labelJsx = <label>{props.label}</label>;
 
-  return (
+  return props.data.saved ? (
+    <div className="form-question">
+      {labelJsx}
+      <span>{props.data.value}</span>
+    </div>
+  ) : (
     <form className="form-question" onSubmit={props.lineInputSave}>
       <label>{props.label}</label>
-      {inputJsx}
+      <input
+        type={props.type}
+        className="hover-highlight"
+        value={props.data.value}
+        state={props.state}
+        onChange={props.lineInputChange}
+        required
+      />
       <SaveButton />
     </form>
   );
