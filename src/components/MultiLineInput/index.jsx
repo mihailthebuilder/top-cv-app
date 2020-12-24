4c2df02745c5fd488caccacbf3688cfd95873a4f
Arkadiusz <arkadiusz@ostrowski.co>
2020-12-24 14:37:55 +0000

    destructure MultiLIneInput

--- src/components/MultiLineInput/index.jsx
+++ src/components/MultiLineInput/index.jsx
@@ -2,27 +2,24 @@ import React from "react";
 //pretty much a copy of ./src/LineInput/LineInput.js, with exception of textarea element replacing input. Used for main responsibilities section.
 import "./MultiLineInput.scss";
 
-const MultiLineInput = (props) => {
-  const labelJsx = <label className="regular-font-size">{props.label}</label>;
-
-  return props.saved ? (
+const MultiLineInput = ({ label, saved, inputValue, inputkey, inputChange }) =>
+  saved ? (
     <div className="textarea-question">
-      {labelJsx}
-      <span className="regular-font-size">{props.inputValue}</span>
+      <label className="regular-font-size">{label}</label>
+      <span className="regular-font-size">{inputValue}</span>
     </div>
   ) : (
     <div className="textarea-question">
-      {labelJsx}
+      <label className="regular-font-size">{label}</label>
       <textarea
         className="hover-highlight regular-font-size border-radius border-width"
         rows="4"
-        value={props.inputValue}
-        inputkey={props.inputkey}
-        onChange={props.inputChange}
+        value={inputValue}
+        inputkey={inputkey}
+        onChange={inputChange}
         required
       />
     </div>
   );
-};
 
 export default MultiLineInput;
